c50fcd89ba5417cda70b8b80536d9f792d1cabdc
Clement Gerardin <clementgerardin.cg@gmail.com>
2021-09-17 11:34:21 +0200

    Ajout script accueil

--- js/script.js
+++ js/script.js
@@ -10,22 +10,46 @@ fetch("http://localhost:3000/api/teddies")
         return product(teddys);
     })
     .catch(function(err) {
-        console.log("erreur")
+        console.log(err)
     });
 
-    function product(teddys)
-    {
-        var elt_Img = document.createElement("img");
-        var elt_Article = document.querySelector("article");
-        var elt_Legende = document.querySelector(".article__legende");
-        elt_Img.setAttribute("alt", "Ours en peluche");
-        elt_Img.classList.add("article__img");
-        var teddy_name = document.querySelector(".article__name")
+var elt_Shop = document.querySelector(".shop");
 
-        teddys.forEach(element => {
-            
-            elt_Article.insertBefore(elt_Img, elt_Legende);
-            elt_Img.setAttribute('src', element["imageUrl"]);
-            teddy_name.textContent = element["name"];
-        });
-    }
+  
+
+function product(teddys)
+{           
+    teddys.forEach(element => {
+        var elt_Article = document.createElement("article");
+        var elt_Img = document.createElement("img");
+        var elt_Div = document.createElement("div");
+        var elt_H2 = document.createElement("h2");
+        var elt_Description = document.createElement("p");
+        var elt_Price = document.createElement("p");
+
+                elt_Article.classList.add("article");
+                elt_Img.classList.add("article__img");
+                elt_Div.classList.add("article__legende");
+                elt_H2.classList.add("article__name");
+                elt_Description.classList.add("article__description");
+                elt_Price.classList.add("article__price");
+
+                elt_Img.setAttribute("alt", "Ours en peluche");
+                elt_Img.setAttribute('src', element["imageUrl"]);
+                
+                elt_Shop.appendChild(elt_Article);
+                elt_Article.appendChild(elt_Img);
+                elt_Article.appendChild(elt_H2);
+                elt_Article.appendChild(elt_Description);
+                elt_Article.appendChild(elt_Price)    
+
+                    elt_H2.textContent = element["name"];
+                    elt_Description.textContent = element["description"];
+                    elt_Price.textContent = element["price"];
+
+    });
+}
+
+
+
+                 
