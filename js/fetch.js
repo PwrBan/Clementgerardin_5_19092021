f2576aa8d085a035928cce0ff65c55dbb004316e
Clement Gerardin <clementgerardin.cg@gmail.com>
2021-09-21 12:16:17 +0200

    Create fetch.js

---
+++ js/fetch.js
@@ -0,0 +1,13 @@
+fetch("http://localhost:3000/api/teddies")
+    .then(function(res) {
+        if(res.ok) {
+            return res.json();
+        }
+    })
+    .then(function teddys_Object(value) {
+        console.log(value[0]._id);
+        return product(value);
+    })
+    .catch(function(err) {
+        console.log(err)
+    });
