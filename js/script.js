77689e17ce3fac6e09166a157c2ef9f3e3040909
Clement Gerardin <clementgerardin.cg@gmail.com>
2021-09-21 12:15:56 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,11 @@
-fetch("http://localhost:3000/api/teddies")
-    .then(function(res) {
-        if(res.ok) {
-            return res.json();
-        }
-    })
-    .then(function(value) {
-        var teddys = value;
-        console.log(teddys);
-        return product(teddys);
-    })
-    .catch(function(err) {
-        console.log(err)
-    });
-
 var elt_Shop = document.querySelector(".shop");
 
-  
-
-function product(teddys)
+function product(value)
 {           
-    teddys.forEach(element => {
+    value.forEach(element => {
         var elt_Link = document.createElement("a"); 
         var elt_Article = document.createElement("article");
+        var elt_DivImg = document.createElement("div")
         var elt_Img = document.createElement("img");
         var elt_Div = document.createElement("div");
         var elt_H2 = document.createElement("h2");
@@ -30,6 +14,7 @@ function product(teddys)
 
                 elt_Link.classList.add("article__link");
                 elt_Article.classList.add("article");
+                elt_DivImg.classList.add("article__div-img")
                 elt_Img.classList.add("article__img");
                 elt_Div.classList.add("article__legende");
                 elt_H2.classList.add("article__name");
@@ -38,19 +23,20 @@ function product(teddys)
 
                 elt_Img.setAttribute("alt", "Ours en peluche");
                 elt_Img.setAttribute('src', element["imageUrl"]);
-                elt_Link.setAttribute("href", "#");
+                elt_Link.setAttribute("href", "product.html?id=" + element["_id"]);
                 
                 elt_Shop.appendChild(elt_Link);
                 elt_Link.appendChild(elt_Article);
-                elt_Article.appendChild(elt_Img);
-                elt_Article.appendChild(elt_H2);
+                elt_Article.appendChild(elt_DivImg);
+                elt_DivImg.appendChild(elt_Img);
+                elt_Article.appendChild(elt_Div);
+                elt_Div.appendChild(elt_H2);
                 /*elt_Article.appendChild(elt_Description);*/
-                elt_Article.appendChild(elt_Price)    
+                elt_Div.appendChild(elt_Price)    
 
                     elt_H2.textContent = element["name"];
                     /*elt_Description.textContent = element["description"];*/
                     elt_Price.textContent = element["price"] + "€";
-
     });
 }
 
