85d5ee8ab8e913407a66ae2d7177aa4cd59f2c91
Clement Gerardin <clementgerardin.cg@gmail.com>
2021-09-29 11:03:16 +0200

    Affichage du panier

--- js/create-cart.js
+++ js/create-cart.js
@@ -1,22 +1,44 @@
+
 function createCartPagesElt(value){
     let keys = Object.values(localStorage);
+    console.log(keys);
     keys.forEach(key => {
-        const find = value.find(value => key === value["_id"]);
-
+        const findId = value.find(value => key === value["_id"]);
+        if (findId === undefined) {
+            console.log("error");
+        }
+        else
+        {        
+        console.log(findId);
+        console.log(key);
+        let qte = localStorage.getItem("qt" + findId["name"]);
+        console.log(qte);
+        let totalPriceTeddy = qte * findId["price"];
         let articleCartElt = document.createElement("article");
         let imgElt = document.createElement("img");
         let divLegendeElt = document.createElement("div");
         let h2NameElt = document.createElement("h2");
+        let qteElt = document.createElement("p");
+        let addBtnElt = document.createElement("button");
+        let removeBtnElt = document.createElement("button");
         let priceElt = document.createElement("p");
         let descriptionElt = document.createElement("p");
-
+        
         imgElt.setAttribute("alt", "Ours en peluche" + value["name"]);
-        imgElt.setAttribute('src', find["imageUrl"]);
+        imgElt.setAttribute('src', findId["imageUrl"]);
+        addBtnElt.setAttribute("onClick", "addArticle('" + findId["name"] + "')");
+        addBtnElt.setAttribute("data-name", findId["name"]);
+        removeBtnElt.setAttribute("onClick", "removeArticle('" + findId["name"] + "')");
+        removeBtnElt.setAttribute("data-name", findId["name"]);
+        qteElt.setAttribute("id", findId["name"].replaceAll(" ", "_"))
 
-        articleCartElt.classList.add("cart-elt__article");
+        articleCartElt.classList.add("cart-elt");
         imgElt.classList.add("cart-elt__img");
         divLegendeElt.classList.add("cart-elt__legende");
         h2NameElt.classList.add("cart-elt__name");
+        qteElt.classList.add("cart-elt__qte");
+        addBtnElt.classList.add("cart-elt__add");
+        removeBtnElt.classList.add("cart-elt___remove");
         priceElt.classList.add("cart-elt__price");
         descriptionElt.classList.add("cart-elt__description");
 
@@ -24,12 +46,42 @@ function createCartPagesElt(value){
         articleCartElt.appendChild(imgElt);
         articleCartElt.appendChild(divLegendeElt);
         divLegendeElt.appendChild(h2NameElt);
+        divLegendeElt.appendChild(qteElt);
+        divLegendeElt.appendChild(addBtnElt);
+        divLegendeElt.appendChild(removeBtnElt);
         divLegendeElt.appendChild(priceElt);
         divLegendeElt.appendChild(descriptionElt);
 
-        h2NameElt.textContent = find["name"];
-        priceElt.textContent = find["price"];
-        descriptionElt.textContent = find["description"];
+        h2NameElt.textContent = findId["name"];
+        priceElt.textContent = totalPriceTeddy + "€";
+        descriptionElt.textContent = findId["description"];
+        qteElt.textContent = "Quantité : " + qte;
+        addBtnElt.textContent = "+";
+        removeBtnElt.textContent = "-";
 
+        }
     });
+}
+
+
+function addArticle(name){
+    console.log(name);
+    let qteTeddys = parseInt(Object.values(localStorage.getItem("qt" + name)));
+    qteTeddys++;
+    localStorage.setItem("qt" + name, qteTeddys);
+    document.querySelector("#" + name.replaceAll(" ", "_")).textContent = "Quantité : " + qteTeddys;
+    
+}
+function removeArticle(name){
+    let qteTeddys = parseInt(Object.values(localStorage.getItem("qt" + name)));
+    qteTeddys--;
+    localStorage.setItem("qt" + name, qteTeddys);
+    document.querySelector("#" + name.replaceAll(" ", "_")).textContent = "Quantité : " + qteTeddys;
+}
+
+function calucateTotalPrice(totalPriceTeddy){
+    localStorage.setItem("total-price", totalPriceTeddy);
+    let x = parseInt(Object.values(localStorage.getItem("total-price")));
+    let total = totalPriceTeddy + x;
+    localStorage.setItem("total-price", total);
 }
--- js/get-order.js
+++ js/get-order.js
@@ -1,13 +1,28 @@
 let orderBtn = document.querySelector(".product__order");
 const cart = document.querySelector(".panier");
-let x = 0;
+let nbArticle = cart.textContent;
 orderBtn.addEventListener("click", () =>
 {
     let idBtn = orderBtn.dataset.id;
     let nameBtn = orderBtn.dataset.name;
-    x++;
-    localStorage.setItem(nameBtn + x, idBtn);
-    cart.textContent = x;
+    localStorage.setItem(nameBtn, idBtn);
+    
+    if (localStorage.getItem("nbArticle")) {
+        console.log(nbArticle)
+        nbArticle++;
+        localStorage.setItem("nbArticle", nbArticle);
+        cart.textContent = Object.values(localStorage.getItem("nbArticle"));
+    }
+    else {
+        localStorage.setItem("nbArticle", 1);
+    };
+    /* Condition pour gérer les doubles dans le panier*/
+    if (localStorage.getItem(nameBtn) && localStorage.getItem("qt" + nameBtn)) {
+        let qtTeddys = parseInt(Object.values(localStorage.getItem("qt" + nameBtn)));
+        qtTeddys++;
+        localStorage.setItem("qt" + nameBtn, qtTeddys);
+    }
+    else {
+        localStorage.setItem("qt" + nameBtn, 1);
+    }
 })
-
-
